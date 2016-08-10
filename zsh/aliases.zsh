b14b66aa4df9f0550fa428912b2fda2687dacd65
viceversus <kenshimizu88@gmail.com>
2016-08-10 11:11:07 -0700

    add react-native alias

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -4,3 +4,6 @@ alias farmers='cd ~/Workspace/Farmers'
 alias farmers-git='ssh -N -L 9001:c5-git.local:9001 c5-proxy'
 alias farmers-wiki='ssh -N -L 4567:ui-farmers.local:4567 c5-proxy'
 alias farmers-ci='ssh -N -L 8080:mobile-ci.local:8080 c5-proxy'
+
+alias be="bundle exec "
+alias rn="react-native "
