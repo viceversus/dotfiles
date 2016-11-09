b693c51bfaa5b59010412eb42c052e9dc0b88086
viceversus <kenshimizu88@gmail.com>
2016-11-09 10:35:49 -0800

    Some updates

--- android/path.zsh
+++ android/path.zsh
@@ -1,3 +1,4 @@
 export ADB_PATH=/Users/ken/Workspace/Android/sdk/platform-tools
+export EMULATOR_PATH=/Users/ken/Workspace/Android/sdk/tools/emulator
 export ANDROID_HOME=/Users/ken/Workspace/Android/sdk
-export PATH="$ADB_PATH:$PATH"
+export PATH="$ANDROID_HOME:$ADB_PATH:$PATH"
--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -6,3 +6,4 @@ alias farmers-wiki='ssh -N -L 4567:ui-farmers.local:4567 c5-proxy'
 alias farmers-ci='ssh -N -L 8080:mobile-ci.local:8080 c5-proxy'
 
 alias be="bundle exec "
+alias sourcetree='open -a SourceTree'
