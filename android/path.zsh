3379986a56d4ef78511abc3c5047907168bb828e
viceversus <kenshimizu88@gmail.com>
2016-12-12 16:57:29 -0800

    fix android path

--- android/path.zsh
+++ android/path.zsh
@@ -1,4 +1,4 @@
 export ADB_PATH=/Users/ken/Workspace/Android/sdk/platform-tools
 export EMULATOR_PATH=/Users/ken/Workspace/Android/sdk/tools/emulator
 export ANDROID_HOME=/Users/ken/Workspace/Android/sdk
-export PATH="$ANDROID_HOME:$ADB_PATH:$PATH"
+export PATH="$ANDROID_HOME/tools:$ANDROID_HOME/platfrom-tools:$PATH"
