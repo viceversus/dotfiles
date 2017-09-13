a95d726ce9d6ebc30eabce6f7b36588397573a14
viceversus <kenshimizu88@gmail.com>
2017-09-13 10:32:45 -0700

    Update some editor settings

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -1,11 +1,7 @@
 alias reload!='. ~/.zshrc'
 
-alias farmers='cd ~/Workspace/Farmers'
-alias farmers-git='ssh -N -L 9001:c5-git.local:9001 c5-proxy'
-alias farmers-wiki='ssh -N -L 4567:ui-farmers.local:4567 c5-proxy'
-alias farmers-ci='ssh -N -L 8080:mobile-ci.local:8080 c5-proxy'
-
 alias be="bundle exec "
 alias sourcetree='open -a SourceTree'
+alias vscode='open -a "Visual Studio Code"'
 
 alias brandless='cd ~/Workspace/Brandless/brandless.com'
